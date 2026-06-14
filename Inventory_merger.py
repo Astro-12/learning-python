52c41a5d37e73ede41607690e1bbf7af9eee98f4
Lunacy <gettoworkritik12@gmail.com>
2026-06-14 23:04:44 +0530

    Add inventory merger functionality
    Implement inventory merger that combines two storehouses and sums quantities of common items.

---
+++ Inventory_merger.py
@@ -0,0 +1,30 @@
+#Making a inventory merger that merges the inventory of two storehouse together to make a combined unique inventory.
+#if a same item exists in both storehouses, the quantity of that item will be added together.
+
+store_1 = {
+    "apples": 10,
+    "oranges": 5,
+    "bananas": 8,
+    "grapes": 12,
+    "milk" : 20
+}
+
+store_2 = {
+    "chocolate": 15,
+    "cookies": 25,
+    "apples": 5,
+    "oranges": 10,
+    "milk" : 10,
+    "Energy Drink" : 30
+}
+
+def merge_inventories(store_1, store_2):
+    merged_inventory = {
+        inventory : store_1.get(inventory, 0) + store_2.get(inventory, 0)
+        for inventory in set(store_1) | set(store_2)
+    }
+
+    return merged_inventory
+
+
+print(merge_inventories(store_1, store_2))
